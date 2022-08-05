720414a4ba8104783019a5cb0622f11c416690e6
Soliha <solyatursuntasheva@gmail.com>
2022-08-05 13:22:50 +0500

    animation-add-gugs-fixed

--- script.js
+++ script.js
@@ -20,3 +20,17 @@ const toggleMobileMenu = function (menu) {
     headerBtn.style.display = "flex";
   }
 };
+window.onscroll = function () {
+  myFunction();
+};
+
+var header = document.getElementById('header');
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
