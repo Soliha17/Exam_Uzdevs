904f9d662fa8e27630ec07a2eaf0c1d29b468d5d
Soliha <solyatursuntasheva@gmail.com>
2023-01-21 12:09:33 +0500

    Change some things

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ window.onscroll = function () {
   myFunction();
 };
 
-var header = document.getElementById('header');
+var header = document.querySelector('.wrapper');
 var sticky = header.offsetTop;
 
 function myFunction() {
@@ -33,4 +33,27 @@ function myFunction() {
   } else {
     header.classList.remove("sticky");
   }
+}
+
+
+// FOR BACK TO TOP BUTTON
+
+// Get the button
+let mybutton = document.getElementById("backToTop");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
